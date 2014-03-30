851fc1143286e96867bf51698b14df1789d66695
Andrelug <pamasway@gmail.com>
2014-03-30 17:36:03 -0300

    initial animations for the articles

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,16 +4,28 @@ var container = $('#gridArticles');
 
 $(window).on({
     scroll:function(){ // fires when user scrolls
-        var current_position = window.pageYOffset; // get the current window Y-Position
-        if( current_position > nav_sticky ) {
-            nav.addClass('sticky');
-            container.css('margin-top',137); // add class to make the nav sticky using css
-        } else {
-            nav.removeClass('sticky');
-            container.css('margin-top',0); // remove sticky css class
+        if($(window).width() > 641){
+            var current_position = window.pageYOffset; // get the current window Y-Position
+            if( current_position > nav_sticky ) {
+                nav.addClass('sticky');
+                container.css('margin-top',137); // add class to make the nav sticky using css
+            } else {
+                nav.removeClass('sticky');
+                container.css('margin-top',0); // remove sticky css class
+            }
         }
     }
 });
+
+
+$('.item').hover(function () {
+    $(this).find('.hidden').css('opacity', 1);
+    $(this).find('h2').addClass('titleHover')
+}, function(){
+    $(this).find('.hidden').css('opacity', 0);
+    $(this).find('h2').removeClass('titleHover')
+});
+
 
 $('#mainInput').keypress(function (event) {
     if (event.keyCode == 13) {
@@ -30,6 +42,7 @@ $(document).on('click', '.tag', function () {
 
 container.packery({
     itemSelector: '.item',
-    gutter: 10
+    gutter: 20,
+    rowHeight: 10
 });
 var pckry = container.data('packery');
