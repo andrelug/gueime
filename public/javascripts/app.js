3fb8755df8c6c407bc49b759e06a22ef6cddfc0f
Andrelug <pamasway@gmail.com>
2014-03-30 18:00:54 -0300

    wait for images load

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,21 +1,6 @@
-var nav = $('#nav-sticky'),
-    nav_sticky = nav.offset().top; //get the Y-position of section
 var container = $('#gridArticles');
 
-$(window).on({
-    scroll:function(){ // fires when user scrolls
-        if($(window).width() > 641){
-            var current_position = window.pageYOffset; // get the current window Y-Position
-            if( current_position > nav_sticky ) {
-                nav.addClass('sticky');
-                container.css('margin-top',137); // add class to make the nav sticky using css
-            } else {
-                nav.removeClass('sticky');
-                container.css('margin-top',0); // remove sticky css class
-            }
-        }
-    }
-});
+
 
 
 $('.item').hover(function () {
@@ -39,10 +24,35 @@ $(document).on('click', '.tag', function () {
     $("#" + id).parent().remove();
 });
 
+container.waitForImages( function () {
+    $("#spinning").hide();
+    container.animate({'opacity': 1});
 
-container.packery({
-    itemSelector: '.item',
-    gutter: 20,
-    rowHeight: 10
+    container.packery({
+        itemSelector: '.item',
+        gutter: 20,
+        rowHeight: 10
+    });
+    var pckry = container.data('packery');
+
+    var nav = $('#nav-sticky');
+    nav_sticky = nav.offset().top; //get the Y-position of section
+
+    $(window).on({
+    scroll:function(){ // fires when user scrolls
+        if($(window).width() > 641){
+            var current_position = window.pageYOffset; // get the current window Y-Position
+            if( current_position > nav_sticky ) {
+                nav.addClass('sticky');
+                container.css('margin-top',137); // add class to make the nav sticky using css
+            } else {
+                nav.removeClass('sticky');
+                container.css('margin-top',0); // remove sticky css class
+            }
+        }
+    }
 });
-var pckry = container.data('packery');
+
+
+
+});
