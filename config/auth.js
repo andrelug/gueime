8f6d8c68238d3519240c45de4492b915d530e385
André Lucas <andre@andrelug.com>
2014-12-02 17:04:54 -0200

    Novo pc e adsense

--- config/auth.js
+++ config/auth.js
@@ -8,19 +8,19 @@ if('development' == app.get('env')) {
 	    'facebookAuth' : {
 		    'clientID' 		: '561543940541408', // your App ID
 		    'clientSecret' 	: 'd550945b38741f8f0dde17027270218e', // your App Secret
-		    'callbackURL' 	: 'http://localhost:55345/auth/facebook/callback'
+		    'callbackURL' 	: 'http://localhost:24279/auth/facebook/callback'
 	    },
 
 	    'twitterAuth' : {
 		    'consumerKey' 		: 'd7Nm9kGmddTbp8bVPd3IRWpAq',
 		    'consumerSecret' 	: 'x2FJbmrix0HaHd0HXWpw5mLVbRvmalCnWLc83bxv481KMd9QeL',
-		    'callbackURL' 		: 'http://localhost:55345/auth/twitter/callback'
+		    'callbackURL' 		: 'http://localhost:24279/auth/twitter/callback'
 	    },
 
 	    'googleAuth' : {
 		    'clientID' 		: '255367409904.apps.googleusercontent.com',
 		    'clientSecret' 	: '99dda7gFKNaONJL6rg23odNZ',
-		    'callbackURL' 	: 'http://localhost:55345/auth/google/callback'
+		    'callbackURL' 	: 'http://localhost:24279/auth/google/callback'
 	    }
 
     };
