658c1a21e4781654d877c29466fa27854aece573
André Lucas <andre@andrelug.com>
2015-12-04 00:39:00 -0200

    ssl3

--- app/routes.js
+++ app/routes.js
@@ -74,7 +74,8 @@ module.exports = function (app, passport, mongoose) {
 
     // SSL
     app.get('/.well-known/acme-challenge/kYiBCLdcXZpyA71igv8tNew-zj1TAY7LJ8eGvtwFVdg', function(req, res){
-        res.write('kYiBCLdcXZpyA71igv8tNew-zj1TAY7LJ8eGvtwFVdg.2vZDavkigD2NRsfb7qwyQRgYGOjyCt8E9niuwb8ye2g');
+        res.type('text/plain');
+        res.send('kYiBCLdcXZpyA71igv8tNew-zj1TAY7LJ8eGvtwFVdg.2vZDavkigD2NRsfb7qwyQRgYGOjyCt8E9niuwb8ye2g');
     });
 
     // =====================================
