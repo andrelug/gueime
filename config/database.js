e42b5ba4ace956706f2d49ab8362fe1f32b1be2f
André Lucas <pamasway@gmail.com>
2016-12-01 21:50:49 -0200

    Forest fix

--- config/database.js
+++ config/database.js
@@ -5,11 +5,13 @@ var express = require('express'),
 if('development' == app.get('env')) {
     module.exports = {
         'url': 'mongodb://localhost/gueime',
-        'url2': 'mongodb://localhost/gueimesessions'
+        'url2': 'mongodb://localhost/gueimesessions',
+		'forest': 'da330be54621c0185d0d1871aba7f18a0c8d24a92dd50971a8e4cfcb7fba6a00'
     }
 }else{
     module.exports = {
         'url': process.env.CUSTOMCONNSTR_MONGOLAB_URI,
-        'url2': process.env.CUSTOMCONNSTR_MONGOLAB_URIA
+        'url2': process.env.CUSTOMCONNSTR_MONGOLAB_URIA,
+		'forest': process.env.CUSTOMCONNSTR_FOREST
     }
 }
